7356649f9a61247c6959c3f1197abc4616ebd2d1
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-08-20 13:06:19 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -19,14 +19,14 @@ PREV_REPLY_MESSAGE = {}
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-CUSTOM_MIDDLE_PMP = str(CUSTOM_PMPERMIT) if CUSTOM_PMPERMIT else "**YOU HAVE TRESPASSED TO MY MASTERS INBOX** \n`THIS IS ILLEGAL AND REGARDED AS A CRIME`"
-USER_BOT_WARN_ZERO = "`You were spamming my Boss's inbox, henceforth your retarded lame ass has been blocked by my master's userbot.` "
-USER_BOT_NO_WARN = ("**Hello !** This is **F.R.I.D.A.\n"
-                    "`Private Messaging Security Protocol ⚠️`\n\n"
-                    "**Currently My Boss**\n"
-                    f"{DEFAULTUSER} is Busy ! So Better Don't Spam His Inbox !\n\n"
+CUSTOM_MIDDLE_PMP = str(CUSTOM_PMPERMIT)"
+USER_BOT_WARN_ZERO = " "
+USER_BOT_NO_WARN = ("**Hello !** __💬 Messaging is not yet Authorised by you.\n"
+                    "`😏 Cool, still there is a way to get Authorisation for Message`\n\n"
+                    "**Currently **\n"
+                    f"{DEFAULTUSER} is Offline or may be Busy ! So Better Don't Spam His Inbox !\n\n"
                     f"{CUSTOM_MIDDLE_PMP} \n\n"
-                    "**Now You Are In Trouble So Send** 🍁 `/start` 🍁  **To Start A Valid Conversation!!**")
+                    "**Now You Are In Trouble So Send** 🍁 `/start` 🍁  **To Authorise, 💬 Messages!!**")
 
 
 if Var.PRIVATE_GROUP_ID is not None:
