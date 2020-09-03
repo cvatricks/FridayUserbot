90c029542dd452ac21146bc9726c885d720ccb40
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-09-04 00:16:42 +0530

    Update zee5.py

--- userbot/plugins/zee5.py
+++ userbot/plugins/zee5.py
@@ -16,35 +16,36 @@ from google_images_download import google_images_download
 from gsearch.googlesearch import search
 from userbot.utils import admin_cmd
 from search_engine_parser import GoogleSearch
+import requests
+import bs4
+import re
 
 
 def progress(current, total):
     logger.info("Downloaded {} of {}\nCompleted {}".format(current, total, (current / total) * 100))
 
 
+
+
+
+
 @borg.on(admin_cmd("zee5 (.*)"))
 async def gsearch(q_event):
     """ For .google command, do a Google search. """
     match = q_event.pattern_match.group(1)
-    page = findall(r"page=\d+", match)
-    try:
-        page = page[0]
-        page = page.replace("page=", "")
-        match = match.replace("page=" + page[0], "")
-    except IndexError:
-        page = 1
-    search_args = (str(match), int(page))
-    gsearch = GoogleSearch()
-    gresults = await gsearch.async_search(*search_args)
-    msg = ""
-    for i in range(len(gresults["links"])):
-        try:
-            title = gresults["titles"][i]
-            link = gresults["links"][i]
-            desc = gresults["descriptions"][i]
-            msg += f"[{title}]({link})\n`{desc}`\n\n"
-        except IndexError:
-            break
-    await q_event.edit("**Search Query:**\n`" + match + "`\n\n**Results:**\n" +
-                       msg,
+    url = match
+    t = "https://useraction.zee5.com/tokennd/"
+    q = requests.get(t)
+    t1 = (eval(q.text)["video_token"])
+    r = requests.get(url)
+    b = bs4.BeautifulSoup(r.content.decode('utf-8'), "html5lib")
+    c = b.find_all("script")[9].prettify()
+    G = []
+    for i in c.split(","):
+      if "hls" in i:
+        G.append(i)
+    #pr = ((G[-1])[11:-1]).replace("drm", "hls")
+    #te = ("https://{}".format("zee5vodnd.akamaized.net") + pr + t1)
+    pr = ("https://zee5vodnd.akamaized.net{}".format((G[-1])[11:-1] + t1).replace("drm", "hls"))
+    await q_event.edit(pr,
                        link_preview=False)
